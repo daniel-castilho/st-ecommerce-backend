396dab84478f9850d3dc5025520973b9eb04dc38
Daniel Castilho <dan.castilho@gmail.com>
2021-03-23 22:49:21 -0400

    Set up Strapi, Connect Strapi to Next.js, Making authenticated requests to Strapi, and Storing Orders Securely

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7979cd0b46969300ebb43779ba6b5c19'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ed836e82-53a3-48df-9943-5fe6e8f71fbb'
+};
